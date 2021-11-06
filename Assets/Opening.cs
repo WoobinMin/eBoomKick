92eaedbbca1ba98f5fcb78aec2c5f4a0e512f327
PakaDopa <detols33@naver.com>
2021-11-07 05:32:38 +0900

    [Add] Opening Scene, Main Scene

---
+++ Assets/Opening.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Playables;
+
+public class Opening : MonoBehaviour
+{
+    public PlayableDirector director;
+    // Start is called before the first frame update
+    void Start()
+    {
+        director.gameObject.SetActive(true);
+        director.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (director.state == PlayState.Paused)
+            SceneManager.LoadScene("MainScene");
+    }
+}
--- Assets/Script/Util/TextEffect.cs
+++ Assets/Script/Util/TextEffect.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using DG.Tweening;
@@ -8,15 +9,22 @@ using DG.Tweening;
 public class TextEffect : MonoBehaviour
 {
     public string gameSceneName;
+    public PlayableDirector director;
     // Start is called before the first frame update
     void Start()
     {
-        this.transform.DOScale(1.1f, 0.5f).SetLoops(-1,LoopType.Yoyo);
+        transform.DOScale(1.1f, 0.5f).SetLoops(-1,LoopType.Yoyo);
+        director.gameObject.SetActive(true);
+        director.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKey) { SceneManager.LoadScene(gameSceneName); }
+        if (director.state == PlayState.Paused && Input.anyKey) {
+            director.gameObject.SetActive(false);
+            transform.DOKill();
+            SceneManager.LoadScene(gameSceneName); 
+        }
     }
 }
